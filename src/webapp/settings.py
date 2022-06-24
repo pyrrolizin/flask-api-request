55fee2dcf91d41b695da157eb0fd4939098a8a24
pyrrolizin <piti.x@gmx.net>
2022-06-24 16:54:37 +0200

    doc: add comments and documentation

--- src/webapp/settings.py
+++ src/webapp/settings.py
@@ -1,5 +1,13 @@
+"""Settings file for development / production"""
+
 from os import environ
 
+#: flask secret key for (future) session handling
 SECRET_KEY = environ.get("SECRET_KEY")
+
+#: API-Key for Openweathermap
 API_KEY = environ.get("API_KEY")
+
+#: Link to the openweathernet-API with city name.
+#: Template-String: {} will be replaced with API-Key. """
 API_URL = "https://api.openweathermap.org/data/2.5/weather?q=Stuttgart,de&units=metric&APPID={}"
--- src/webapp/settings_testing.py
+++ src/webapp/settings_testing.py
@@ -1,6 +1,10 @@
+"""Settings file for pytest"""
 from os import environ
 import secrets
 
+#: Random flask secret key
 SECRET_KEY = secrets.token_hex()
+
+#: Activate mocking of API
 API_KEY = "MOCKING"
 API_URL = "MOCK"
